0e4fe1aecd0ab4d61a70a914288ddc133f34e173
koyie <102526406+Koyiee@users.noreply.github.com>
2025-07-02 21:25:08 +0800

    profile picture

--- resources/js/components/Patients/PatientDentalPhotos.vue
+++ resources/js/components/Patients/PatientDentalPhotos.vue
@@ -13,7 +13,28 @@
     <li><a href="/patientbillings"><i class="bx bx-wallet"></i><span class="link_name">Billing</span></a><ul class="sub-menu blank"><li><a class="link_name" href="/patientbillings">Billing</a></li></ul></li>
     <li><div class="icon-link"><a href="/patienttreatments"><i class="bx bx-clipboard"></i><span class="link_name">Treatments</span></a><i class="bx bxs-chevron-down arrow"></i></div><ul class="sub-menu"><li><a class="link_name" href="/patienttreatments">Treatments</a></li><li><a href="/patienttreatmentplan">Treatment Plan</a></li><li><a href="/patientdentalphotos">Dental Photos</a></li><li><a href="/patientforms">Forms</a></li><li><a href="/patientdentaltreatmenthistory">Dental Treatment History</a></li></ul></li>
     <li><a href="/patientsettings"><i class="bx bx-cog"></i><span class="link_name">Settings</span></a><ul class="sub-menu blank"><li><a class="link_name" href="/patientsettings">Settings</a></li></ul></li>
-    <li><div class="profile-details" @click="confirmLogout"><div class="profile-content"><img src="@/components/Images/profile_1.jpg" alt="profile"></div><div class="name-job"><div class="profile_name">{{ fullName }}</div><div class="job">Patient</div></div><i class="bx bx-log-out"></i></div></li>
+    <li><div class="profile-details" @click="confirmLogout">
+            <div class="profile-content">
+              <img 
+                v-if="profilePicture" 
+                :src="`/storage/${profilePicture}`" 
+                alt="profile" 
+                class="profile-image" 
+                @error="handleImageError"
+              >
+              <img 
+                v-else 
+                src="../Images/profile_1.jpg" 
+                alt="profile" 
+                class="profile-image"
+              >
+            </div>
+            <div class="name-job">
+              <div class="profile_name">{{ fullName }}</div>
+              <div class="job">Patient</div>
+            </div>
+            <i class="bx bx-log-out"></i>
+          </div></li>
   </ul>
 </div>
 
@@ -236,9 +257,14 @@ export default {
       type: String,
       required: true,
     },
+    profilePicture: {
+      type: String,
+      default: null,
+    },
   },
   data() {
     return {
+      profilePicture: sessionStorage.getItem('user_profile_picture') || this.profilePicture,
       dentalPhotos: [],
       filteredDentalPhotos: [],
       searchQuery: '',
@@ -272,6 +298,43 @@ export default {
     document.removeEventListener('click', this.handleClickOutside);
   },
   methods: {
+      async fetchPatientData() {
+    try {
+      const response = await axios.get('/api/patient-data');
+      if (response.data.success) {
+        this.profilePicture = response.data.patient.profilePicture;
+        console.log('Fetched Profile Picture:', this.profilePicture);
+      } else {
+        console.error('Failed to fetch patient data:', response.data.message);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Failed to fetch patient data. Please try again.',
+          showConfirmButton: false,
+          timer: 1500,
+          customClass: {
+            popup: 'sweetalert-theme',
+            title: 'sweetalert-title',
+            content: 'sweetalert-content',
+          },
+        });
+      }
+    } catch (error) {
+      console.error('Error fetching patient data:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Error fetching patient data. Please try again.',
+        showConfirmButton: false,
+        timer: 1500,
+        customClass: {
+          popup: 'sweetalert-theme',
+          title: 'sweetalert-title',
+          content: 'sweetalert-content',
+        },
+      });
+    }
+  },
     async confirmLogout() {
       const result = await Swal.fire({
         title: 'Are you sure?',
